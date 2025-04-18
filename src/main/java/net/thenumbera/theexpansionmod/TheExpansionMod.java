f58bd8485f0287d077012d498147528155bbbf6c
TheNumber_A <jeffreyjw1016@gmail.com>
2025-04-18 16:47:46 -0700

    Initial commit

---
+++ src/main/java/net/thenumbera/theexpansionmod/TheExpansionMod.java
@@ -0,0 +1,16 @@
+package net.thenumbera.theexpansionmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheExpansionMod implements ModInitializer {
+	public static final String MOD_ID = "theexpansionmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thenumbera/theexpansionmod/TheExpansionModClient.java
@@ -0,0 +1,10 @@
+package net.thenumbera.theexpansionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheExpansionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
