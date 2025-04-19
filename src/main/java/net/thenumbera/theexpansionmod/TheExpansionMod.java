4e48d2fe2df6a8692b2126ce74edb5ed0f97a57a
TheNumber_A <jeffreyjw1016@gmail.com>
2025-04-18 17:28:51 -0700

    New Update

--- src/main/java/net/thenumbera/theexpansionmod/TheExpansionMod.java
+++ src/main/java/net/thenumbera/theexpansionmod/TheExpansionMod.java
@@ -2,6 +2,7 @@ package net.thenumbera.theexpansionmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.thenumbera.theexpansionmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TheExpansionMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
